d240350cc5edf69d43045ff373a2e81b7f778b70
Shoaib <shoaibhaque7@gmail.com>
2022-07-01 18:30:27 +0600

    Added routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "bootstrap/dist/css/bootstrap.css";
+
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+import EditStatement from "./components/statement/edit.component";
+import StatementList from "./components/statement/list.component";
+import CreateStatement from "./components/statement/create.component";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar bg="primary">
+        <Container>
+          <Link to={"/"} className="navbar-brand text-white">
+            Bank Statement
+          </Link>
+        </Container>
+      </Navbar>
+
+      <Container className="mt-5">
+        <Row>
+          <Col md={12}>
+            <Routes>
+              <Route path="/statement/create" element={<CreateStatement />} />
+              <Route path="/statement/edit/:id" element={<EditStatement />} />
+              <Route path="/statements" element={<StatementList />} />
+            </Routes>
+          </Col>
+        </Row>
+      </Container>
+    </Router>
   );
 }
 
